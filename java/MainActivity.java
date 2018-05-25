9c5e07a0a30433459363e9f8bb10b977a1978ffd
Cecilia3333 <38930809+Cecilia3333@users.noreply.github.com>
2018-05-25 13:29:20 +0800

    dialog

--- java/MainActivity.java
+++ java/MainActivity.java
@@ -1,32 +1,46 @@
-package com.example.button;
+package com.example.dialog;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
+
 public class MainActivity extends Activity {
-	Button btnYellow,btnBlue;
+	private TextView myTV;
+	private Button myBtn;
 	@Override
-	public void onCreate(Bundle savedInstanceState){
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		btnYellow=(Button)this.findViewById(R.id.btnYellow);
-		btnBlue=(Button)this.findViewById(R.id.btnBlue);
-		btnYellow.setOnClickListener(new OnClickListener(){
-			@Override
+		//根据Id获取组件
+		myTV=(TextView)findViewById(R.id.TextView01);
+		myBtn=(Button)findViewById(R.id.Button01);
+		//获得Builder对象
+		final AlertDialog.Builder builder=new AlertDialog.Builder(this);
+		//在按钮上注册监听器
+		myBtn.setOnClickListener(new OnClickListener(){
 			public void onClick(View v){
-				getWindow().setBackgroundDrawableResource(R.color.yellow);
+				//通过builder对象设置对话框信息
+				builder.setMessage("真的要删除该记录吗？")
+				//设置确定按钮及其相应事件处理
+				.setPositiveButton("是", new DialogInterface.OnClickListener(){
+					public void onClick(DialogInterface dialog,int which){
+						myTV.setText("删除成功！");}})
+						//设置取消按钮及其相应事件处理
+				.setNegativeButton("否", new DialogInterface.OnClickListener(){
+					public void onClick(DialogInterface dialog,int which){
+						myTV.setText("取消删除！");}});
+				//创建对话框
+				AlertDialog ad=builder.create();
+				//显示对话框
+				ad.show();
+					}
+				});
 			}
-		});
-		btnBlue.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v){
-				getWindow().setBackgroundDrawableResource(R.color.blue);
-			}
-		});
-	}
-}
-
+		}
